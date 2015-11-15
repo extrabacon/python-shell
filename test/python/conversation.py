d0937debcedd436431f3bba3761f996ee7ba5642
Alex Birch <vengeance.mx@gmail.com>
2015-11-15 18:28:57 +0000

    fix JSON syntax in conversation.py, modularize further, handle more types of unexpected input

--- test/python/conversation.py
+++ test/python/conversation.py
@@ -1,17 +1,24 @@
 import sys, json
 
+def is_json(myjson):
+    try:
+        json_object = json.loads(myjson)
+    except ValueError, e:
+        return False
+    return True
+
 def makeError(reason):
 	return {
-		action: 'error',
-		reason: reason
+		'action': 'error',
+		'reason': reason
 	}
 
 def handleKnockKnock(obj):
 	response = {
-		action: 'knockknockjoke'
+		'action': 'knockknockjoke'
 	};
 
-	phase = obj.phase;
+	phase = obj['phase'];
 	if (phase == 'Knock, knock.'):
 		response.message = "Who's there?"
 		return response;
@@ -27,14 +34,28 @@ def handleKnockKnock(obj):
 	return makeError('Unrecognised knock-knock phase.')
 
 def handleAction(obj):
-	action = obj.action
+	if 'action' not in obj:
+		return makeError("Unsupported input; expected 'action' key.")
+	
+	action = obj['action']
 	if action == 'knockknockjoke':
 		return handleKnockKnock(obj)
 
 	return makeError("Unrecognised action: {0}".format(action))
 
+def handleLine(line):
+	if not is_json(line):
+		return makeError('Malformed input could not be parsed as JSON: {0}'.format(line))
+
+	parsed = json.loads(line)
+
+	if type (parsed) != type({}):
+		return makeError('Malformed input: expected JSON object; received JSON primitive instead: {0}'.format(parsed))
+		
+	return handleAction(parsed)
+
 # simple JSON echo script
 for line in sys.stdin:
-	parsed = json.loads(line)
-	response = handleAction(parsed)
+	response = handleLine(line)
+
 	print json.dumps(response)
