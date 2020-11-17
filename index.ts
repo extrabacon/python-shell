3e3636a71ce651393d7a28c04bfafc79073003af
Emily Marigold Klassen <forivall@users.noreply.github.com>
2020-11-17 13:05:44 -0800

    doc: add typings for events

--- index.ts
+++ index.ts
@@ -432,3 +432,41 @@ export class PythonShell extends EventEmitter{
         return this.kill(signal)
     }
 };
+
+// This interface is merged in with the above class definition
+export interface PythonShell {
+    addListener(event: string, listener: (...args: any[]) => void): this;
+    emit(event: string | symbol, ...args: any[]): boolean;
+    on(event: string, listener: (...args: any[]) => void): this;
+    once(event: string, listener: (...args: any[]) => void): this;
+    prependListener(event: string, listener: (...args: any[]) => void): this;
+    prependOnceListener(event: string, listener: (...args: any[]) => void): this;
+
+    addListener(event: "message", listener: (parsedChunk: any) => void): this;
+    emit(event: "message", parsedChunk: any): boolean;
+    on(event: "message", listener: (parsedChunk: any) => void): this;
+    once(event: "message", listener: (parsedChunk: any) => void): this;
+    prependListener(event: "message", listener: (parsedChunk: any) => void): this;
+    prependOnceListener(event: "message", listener: (parsedChunk: any) => void): this;
+
+    addListener(event: "stderr", listener: (parsedChunk: any) => void): this;
+    emit(event: "stderr", parsedChunk: any): boolean;
+    on(event: "stderr", listener: (parsedChunk: any) => void): this;
+    once(event: "stderr", listener: (parsedChunk: any) => void): this;
+    prependListener(event: "stderr", listener: (parsedChunk: any) => void): this;
+    prependOnceListener(event: "stderr", listener: (parsedChunk: any) => void): this;
+
+    addListener(event: "close", listener: () => void): this;
+    emit(event: "close", ): boolean;
+    on(event: "close", listener: () => void): this;
+    once(event: "close", listener: () => void): this;
+    prependListener(event: "close", listener: () => void): this;
+    prependOnceListener(event: "close", listener: () => void): this;
+
+    addListener(event: "error", listener: (error: PythonShellError) => void): this;
+    emit(event: "error", error: PythonShellError): boolean;
+    on(event: "error", listener: (error: PythonShellError) => void): this;
+    once(event: "error", listener: (error: PythonShellError) => void): this;
+    prependListener(event: "error", listener: (error: PythonShellError) => void): this;
+    prependOnceListener(event: "error", listener: (error: PythonShellError) => void): this;
+}
